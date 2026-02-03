e1ac9a2b9b6af31cb7b9d2b4cbbb91ccad1d603d
Justin <just8394in@gmail.com>
2026-02-03 13:30:40 +0100

    ships van de cpu gemaakt

--- website/js/game.js
+++ website/js/game.js
@@ -14,6 +14,8 @@ if (angle === 0) {
 optionShips.forEach(optionShip => optionShip.style.transform = `rotate(${angle}deg)`);
 }
 
+rotateButton.addEventListener('click', rotate);
+
 
 const width = 10;
 
@@ -24,17 +26,53 @@ function createBoard(color, user){
    gameBoardContainer.id = user;
 
    for(let i = 0; i < width * width; i++){
-    const block = document.createElement('div')
-    block.classList.add('block')
-    block.id = i
-    gameBoardContainer.append(block)
+    const block = document.createElement('div');
+    block.classList.add('block');
+    block.id = i;
+    gameBoardContainer.append(block);
    }
 
-   mainContainer.append(gameBoardContainer);
+    mainContainer.append(gameBoardContainer);
 }
 
-createBoard('blue');
-createBoard('red');
+createBoard('blue', 'player');
+createBoard('red', 'computer');
 
-rotateButton.addEventListener('click', rotate);
+class ship{
+    constructor(name, length){
+        this.name = name;
+        this.length = length;
+    }
+}
 
+const destroyer = new ship('destroyer' , 2);
+const submarine = new ship('submarine' , 3);
+const cruiser = new ship('cruiser' , 3);
+const battleship = new ship('battleship' , 4);
+const carrier = new ship('carrier' , 5);
+
+const allShips = [destroyer, submarine, cruiser, battleship, carrier];
+
+function addShipPiece(ship){
+    const allBoardBlocks = document.querySelectorAll('#computer div');
+    let randomBoolean = Math.random() < 0.5;
+    let isHorizontal = randomBoolean;
+    let randomStartIndex = Math.floor(Math.random() * width * width);
+
+    let shipBlocks = [];
+
+    for(let i = 0; i < ship.length; i++){
+        if (isHorizontal){
+            shipBlocks.push(allBoardBlocks[Number(randomStartIndex) + i]);
+        }else{
+            shipBlocks.push(allBoardBlocks[randomStartIndex + i * width]);
+        }
+    }
+
+    shipBlocks.forEach(shipBlock => {
+        shipBlock.classList.add(ship.name);
+        shipBlock.classList.add('taken');
+    })
+
+}
+allShips.forEach(ship => addShipPiece(ship));
