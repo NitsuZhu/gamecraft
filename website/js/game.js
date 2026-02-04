0bc89e91b3917a482d7d74f5a1cc4f62de96fa90
Justin <just8394in@gmail.com>
2026-02-04 13:37:41 +0100

    ships van de cpu aangepast waardoor ze in de container passen

--- website/js/game.js
+++ website/js/game.js
@@ -59,14 +59,31 @@ function addShipPiece(ship){
     let isHorizontal = randomBoolean;
     let randomStartIndex = Math.floor(Math.random() * width * width);
 
+    let validStart = isHorizontal ? randomStartIndex <= width * width - ship.length ? randomStartIndex :
+        width * width - ship.length :
+
+        randomStartIndex <= width * width - width * ship.length ? randomStartIndex : 
+            randomStartIndex - ship.length * width + width;
+
     let shipBlocks = [];
 
     for(let i = 0; i < ship.length; i++){
         if (isHorizontal){
-            shipBlocks.push(allBoardBlocks[Number(randomStartIndex) + i]);
+            shipBlocks.push(allBoardBlocks[Number(validStart) + i]);
         }else{
-            shipBlocks.push(allBoardBlocks[randomStartIndex + i * width]);
+            shipBlocks.push(allBoardBlocks[Number(validStart) + i * width]);
         }
+    }
+
+    let valid
+    
+    if (isHorizontal) {
+    shipBlocks,every((shipBlock, index) => 
+        valid = shipBlocks[0].id % width !== width - (shipBlocks.length - (index + 1)));
+    } else{
+        shipBlocks.every((_shipBlock, index) =>
+           valid = shipBlocks[0].id < 90 + (width * index + 1); 
+        )
     }
 
     shipBlocks.forEach(shipBlock => {
